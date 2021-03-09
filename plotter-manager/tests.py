89e27144c0decb959c38aac324d650020b2a9ae1
Daniel <daniellsmarta@gmail.com>
2021-03-09 22:44:15 +0100

    [testing] if non-linear, namely squared data after plotting inside the plot is the same

--- plotter-manager/tests.py
+++ plotter-manager/tests.py
@@ -15,3 +15,19 @@ def test_plot_linear():
     line, = plot(ax, x, y)
     x_plot, y_plot = line.get_xydata().T
     np.testing.assert_array_equal(x_plot, y_plot)
+
+
+##########################################################
+# Testing if squared data after plotting with plot and get_axix_fig is equal
+
+
+def plot_square(x, y):
+    y_squared = np.square(y)
+    return plt.plot(x, y_squared)
+
+
+def test_plot_square():
+    x, y = [0, 1, 2], [0, 1, 2]
+    line, = plot_square(x, y)
+    x_plot, y_plot = line.get_xydata().T
+    np.testing.assert_array_equal(y_plot, np.square(y))
