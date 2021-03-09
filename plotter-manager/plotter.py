3842bf8f216bf0f7294f8ae8eb84c95e11cb5e67
Daniel <daniellsmarta@gmail.com>
2021-03-09 18:03:38 +0100

    defining initial functions for our plotter

---
+++ plotter-manager/plotter.py
@@ -0,0 +1,15 @@
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+
+import numpy as np
+
+def 
+
+def plot(x,y):
+    plot(x,y)
+
+def set_labels(axis,x_label, y_label,title):
+    pass
+
+def set_grid()
