ca47cc8172c9cba908b5d6b8afe8b4a1b3ba96ba
Daniel <daniellsmarta@gmail.com>
2021-03-09 18:18:43 +0100

    adding a moving average utility

--- plotter-manager/utils.py
+++ plotter-manager/utils.py
@@ -17,3 +17,23 @@ def return_date():
     now = datetime.now()
     dt_string = now.strftime("%d-%m-%Y %H:%M:%S")
     return dt_string
+
+
+def moving_average(rewards, last_N):
+    scores = []
+    averages = []
+    stds = []
+    for _ in range(len(rewards)):
+        scores.append(rewards[_])
+        average = sum(scores[-last_N:]) / len(scores[-last_N:])
+        averages.append(average)
+        x = scores[-last_N:]
+        x_mean = averages[-last_N:]
+        diff = []
+        for _ in range(len(x)):
+            diff.append((abs(x[_] - x_mean[_]))**(1 / 2))
+        diff_sum = sum(diff)
+
+        stds.append(diff_sum / last_N)
+
+    return averages, stds
