37e65333a49bb79184c85641319d0c7d0c85d96c
Daniel <daniellsmarta@gmail.com>
2021-03-12 18:45:55 +0100

    Extract class/interface/method [3 points]

--- plotter-manager/manager.py
+++ plotter-manager/manager.py
@@ -1,7 +1,16 @@
 from abc import ABC, abstractmethod
 
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+import numpy as np
 
-class PlotterManager(ABC):
+from utils import moving_average, save_pickle, load_pickle
+
+from plot import plot_data
+
+
+class Plotter(ABC):
     def __init__(self):
         pass
 
@@ -30,5 +39,40 @@ class PlotterManager(ABC):
         return NotImplemented
 
 
+class StaticPlotter(Plotter):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.axis = None
+        self.figure = None
+
+    def plot_files(self):
+        raise NotImplementedError
+
+    def plot_from_file(self):
+        raise NotImplementedError
+
+    def plot_data(self, x, y, title):
+        plot_data(x=x, y=t, title=title)
+
+    def get_axis_fig(self):
+        fig, ax = plt.subplots()
+        return fig, ax
+
+    def plot(self, axis, x, y):
+        return axis.plot(x, y)
+
+    def set_labels(self, axis, x_label, y_label, title):
+        axis.set(xlabel=x_label, ylabel=y_label, title=title)
+
+    def set_grid(self, axis):
+        axis.grid()
+
+    def save_figure(self, fig, fig_name):
+        fig.savefig(OUTPUT_DIR + fig_name)
+
+    def show(self):
+        plt.show()
+
+
 if __name__ == '__main__':
-    pass
+    sp = StaticPlotter()
